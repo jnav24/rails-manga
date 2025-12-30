2731be3fdcdcc724fb6c4fe722113744e10695f2
Justin Navarro <info@justinnavarro.net>
2025-12-29 23:12:06 -0500

    add more test cases

--- test/models/model_has_permission_test.rb
+++ test/models/model_has_permission_test.rb
@@ -1,7 +1,49 @@
 require "test_helper"
 
 class ModelHasPermissionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @permission = permissions(:one)
+  end
+
+  test "should be valid with all required attributes" do
+    model_permission = ModelHasPermission.new(
+      permission: @permission,
+      model: @user
+    )
+    assert model_permission.valid?
+  end
+
+  test "should require a permission" do
+    model_permission = ModelHasPermission.new(permission: nil, model: @user)
+    assert_not model_permission.valid?
+    assert_includes model_permission.errors[:permission], "must exist"
+  end
+
+  test "should require a model" do
+    model_permission = ModelHasPermission.new(permission: @permission, model: nil)
+    assert_not model_permission.valid?
+    assert_includes model_permission.errors[:model], "must exist"
+  end
+
+  test "should not allow duplicate permission for same model" do
+    user = User.create!(email_address: "test@test.com", password: "password")
+    permission = Permission.create!(name: "test")
+    ModelHasPermission.create!(permission:, model: user)
+    duplicate = ModelHasPermission.new(permission:, model: user)
+    assert_raises(ActiveRecord::RecordNotUnique) do
+      duplicate.save(validate: false)
+    end
+  end
+
+  test "should allow same permission for different models" do
+    user = User.create!(email_address: "test@test.com", password: "password")
+    user2 = User.create!(email_address: "test2@test.com", password: "password")
+    permission = Permission.create!(name: "test")
+
+    ModelHasPermission.create!(permission:, model: user)
+    model_permission = ModelHasPermission.new(permission:, model: user2)
+
+    assert model_permission.valid?
+  end
 end
--- test/models/model_has_role_test.rb
+++ test/models/model_has_role_test.rb
@@ -1,7 +1,49 @@
 require "test_helper"
 
 class ModelHasRoleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @role = roles(:one)
+  end
+
+  test "should be valid with all required attributes" do
+    mode_role = ModelHasRole.new(
+      role: @role,
+      model: @user
+    )
+    assert mode_role.valid?
+  end
+
+  test "should require a role" do
+    model_role = ModelHasRole.new(role: nil, model: @user)
+    assert_not model_role.valid?
+    assert_includes model_role.errors[:role], "must exist"
+  end
+
+  test "should require a model" do
+    model_role = ModelHasRole.new(role: @role, model: nil)
+    assert_not model_role.valid?
+    assert_includes model_role.errors[:model], "must exist"
+  end
+
+  test "should not allow duplicate role for same model" do
+    user = User.create!(email_address: "test@test.com", password: "password")
+    role = Role.create!(name: "test")
+    ModelHasRole.create!(role:, model: user)
+    duplicate = ModelHasRole.new(role:, model: user)
+    assert_raises(ActiveRecord::RecordNotUnique) do
+      duplicate.save(validate: false)
+    end
+  end
+
+  test "should allow same role for different models" do
+    user = User.create!(email_address: "test@test.com", password: "password")
+    user2 = User.create!(email_address: "test2@test.com", password: "password")
+    role = Role.create!(name: "test")
+
+    ModelHasRole.create!(role:, model: user)
+    model_role = ModelHasRole.new(role:, model: user2)
+
+    assert model_role.valid?
+  end
 end
